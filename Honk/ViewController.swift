15c94b74ac0a2bf02133a81a6fe6a5c45d43b0db
kenkl <kenkl@mac.com>
2020-08-04 12:52:35 -0400

    make the buttons line up correctly; dim the images to provide feedback

--- Honk/ViewController.swift
+++ Honk/ViewController.swift
@@ -11,9 +11,15 @@ import AVFoundation
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var honkOutside: UIImageView!
+    @IBOutlet weak var honkEcho: UIImageView!
+    @IBOutlet weak var honkBottle: UIImageView!
+    @IBOutlet weak var honkMuffled: UIImageView!
+    
     var player: AVAudioPlayer!
     var honkIndex = 0
     var soundName:String? = nil
+    var image:UIImageView? = nil
     
     @IBAction func honkButton(_ sender: UIButton) {
         let buttonPressed = sender.currentTitle!
@@ -25,10 +31,29 @@ class ViewController: UIViewController {
             honkIndex = Int.random(in: 1...3)
         }
         soundName = buttonPressed + String(honkIndex)
-        
 //        print(soundName!)
-        
         playSound(soundName: soundName!)
+        
+        // Map the button names to the UIImageView of the corresponding image.
+        switch buttonPressed {
+        case "Honk":
+            image = honkOutside
+        case "Honk-lid":
+            image = honkEcho
+        case "Honk-glass":
+            image = honkBottle
+        case "Honk-muffled":
+            image = honkMuffled
+        default:
+            print("We should never get here.")
+        }
+        // Then, let's dim the image for a beat to show what's been done.
+        image!.alpha = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+        //Bring's sender's opacity back up to fully opaque.
+            self.image!.alpha = 1.0
+        print("End")
+        }
         
         
         
@@ -41,7 +66,7 @@ class ViewController: UIViewController {
     }
 
     func playSound(soundName: String) {
-        print("playSound got \(soundName)")
+//        print("playSound got \(soundName)")
     let url = Bundle.main.url(forResource: soundName,  withExtension: "wav")
 //        print("playSound url = \(String(describing: url))")
     player = try! AVAudioPlayer(contentsOf: url!)
